3ee2dd6be82138792ab81bf82e588f7d207b01e2
Talitha Souza <litha06h@gmail.com>
2022-04-27 11:40:18 -0300

    Crud e conexão com BD atraves do scaffolding

---
+++ Vendedores_MVC/Data/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vendedores_MVC.Models;
+
+namespace Vendedores_MVC.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext (DbContextOptions<MyContext> options): base(options){ }
+
+        public DbSet<Departamento> Departamento { get; set; }
+    }
+}
---
+++ Vendedores_MVC/Migrations/20220427133332_inicio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vendedores_MVC.Migrations
+{
+    public partial class inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamento", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departamento");
+        }
+    }
+}
