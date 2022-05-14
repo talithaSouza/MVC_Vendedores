d67b4846172703ae0c7433673b1cf1816d88abc7
Talitha <litha06h@gmail.com>
2022-05-14 15:31:50 -0300

    tratamento de exceção para deletar departamento

--- Vendedores_MVC/Controllers/DepartamentosController.cs
+++ Vendedores_MVC/Controllers/DepartamentosController.cs
@@ -1,9 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Vendedores_MVC.Data;
 using Vendedores_MVC.Models;
+using Vendedores_MVC.Models.ViewModels;
+using Vendedores_MVC.Service.Exceptions;
 
 namespace Vendedores_MVC.Controllers
 {
@@ -136,15 +140,33 @@ namespace Vendedores_MVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var departamento = await _context.Departamentos.FindAsync(id);
-            _context.Departamentos.Remove(departamento);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                var departamento = await _context.Departamentos.FindAsync(id);
+                _context.Departamentos.Remove(departamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction(nameof(Error), new { mensagem = "Não é possivel Deletar esse Departamento, pois a Vendedores associados a ele" }); ;
+            }
         }
 
         private bool DepartamentoExists(int id)
         {
             return _context.Departamentos.Any(e => e.Id == id);
+        }
+
+        public IActionResult Error(string mensagem)
+        {
+            var viewModel = new ErrorViewModel()
+            {
+                Mensagem = mensagem,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+
+            return View(viewModel);
         }
     }
 }
